e1e4c0bcbfecf77592dfb993f66375067c7ac0d1
uckuruslukopera <loginquitas@gmail.com>
2017-05-07 22:13:46 +0300

    fixed typo in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = "boiling-dusk-57136.herokuapp.com"
-  config.action_maile.default_url_options = {host: host}
+  config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
